bc48f041f3b90f617d258713b3929506328de48c
Nikolaus Winter <mail@nikolaus-winter.de>
2022-08-23 15:22:31 +0200

    platform test job

--- tools/testing-toolbox/testing-toolbox.py
+++ tools/testing-toolbox/testing-toolbox.py
@@ -2,14 +2,14 @@ import sys
 import create_jenkins_jobs
 import create_testsuite
 import create_platform_test_job
-
+import create_platform_testsuite
 
 if __name__ == "__main__":
 
     print("stackable.tech testing toolbox")
 
-    if(len(sys.argv) < 2 or sys.argv[1] not in ['create-testsuite', 'create-jenkins-jobs', 'create-platform-test-job']):
-        print("Please supply command [create-testsuite, create-jenkins-jobs, create-platform-test-job]")
+    if(len(sys.argv) < 2 or sys.argv[1] not in ['create-testsuite', 'create-jenkins-jobs', 'create-platform-test-job', 'create-platform-testsuite']):
+        print("Please supply command [create-testsuite, create-jenkins-jobs, create-platform-test-job, create-platform-testsuite]")
         exit(1)
 
     if("create-testsuite"==sys.argv[1]):
@@ -20,3 +20,6 @@ if __name__ == "__main__":
 
     if("create-platform-test-job"==sys.argv[1]):
         create_platform_test_job.create_platform_test_job()
+
+    if("create-platform-testsuite"==sys.argv[1]):
+        create_platform_testsuite.create_platform_testsuite()
