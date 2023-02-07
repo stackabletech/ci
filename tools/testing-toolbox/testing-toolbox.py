b5725fb098f6c59c02069da2856b0b760a3c8632
Nikolaus Winter <mail@nikolaus-winter.de>
2023-02-07 09:51:17 +0100

    Testing-Toolbox: create documentation testsuite

--- tools/testing-toolbox/testing-toolbox.py
+++ tools/testing-toolbox/testing-toolbox.py
@@ -1,6 +1,7 @@
 import sys
 import create_jenkins_jobs
 import create_testsuite
+import create_documentation_testsuite
 import create_platform_test_jobs
 import create_platform_testsuite
 
@@ -8,12 +9,15 @@ if __name__ == "__main__":
 
     print("stackable.tech testing toolbox")
 
-    if(len(sys.argv) < 2 or sys.argv[1] not in ['create-testsuite', 'create-jenkins-jobs', 'create-platform-test-job', 'create-platform-testsuite']):
-        print("Please supply command [create-testsuite, create-jenkins-jobs, create-platform-test-job, create-platform-testsuite]")
+    if(len(sys.argv) < 2 or sys.argv[1] not in ['create-testsuite', 'create-docs-testsuite', 'create-jenkins-jobs', 'create-platform-test-job', 'create-platform-testsuite']):
+        print("Please supply command [create-testsuite, create-docs-testsuite, create-jenkins-jobs, create-platform-test-job, create-platform-testsuite]")
         exit(1)
 
     if("create-testsuite"==sys.argv[1]):
         create_testsuite.create_testsuite()
+
+    if("create-docs-testsuite"==sys.argv[1]):
+        create_documentation_testsuite.create_testsuite()
 
     if("create-jenkins-jobs"==sys.argv[1]):
         create_jenkins_jobs.create_jenkins_jobs()
