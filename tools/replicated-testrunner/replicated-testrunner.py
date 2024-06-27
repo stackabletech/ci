55926a0ae761d25c28dec1c33fe56323470bd154
Nikolaus Winter <mail@nikolaus-winter.de>
2024-06-27 10:34:24 +0200

    Replicated: test-suite for Beku no longer set as a default, has to be specified as param

--- tools/replicated-testrunner/replicated-testrunner.py
+++ tools/replicated-testrunner/replicated-testrunner.py
@@ -13,7 +13,7 @@ platform = None
 platform_version = None
 git_branch = None
 operator_version = None
-override_beku_params = ''
+test_script_params = ''
 
 catalog = None
 
@@ -34,9 +34,8 @@ def init():
     global platform_version
     global git_branch
     global catalog
-    global beku_test_suite
     global operator_version
-    global override_beku_params
+    global test_script_params
 
     if not 'REPLICATED_API_TOKEN' in os.environ:
         print("Error: Please supply REPLICATED_API_TOKEN as an environment variable.")
@@ -74,8 +73,8 @@ def init():
     if 'GIT_BRANCH' in os.environ:
         git_branch = os.environ['GIT_BRANCH']
 
-    if 'OVERRIDE_BEKU_PARAMS' in os.environ:
-        override_beku_params = os.environ['OVERRIDE_BEKU_PARAMS']
+    if 'TEST_SCRIPT_PARAMS' in os.environ:
+        test_script_params = os.environ['TEST_SCRIPT_PARAMS']
 
     catalog = hiyapyco.load("/replicated.yaml")
 
@@ -201,25 +200,22 @@ def clone_git_repo(repo):
         return False
     return True
 
-def run_tests(operator, beku_test_suite, operator_version, parallel, override_params):
+def run_tests(operator, operator_version, parallel, test_script_params):
     """ 
     Runs the tests using the test script in the operator repo.
 
-    operator:           name of the operator-repo (usually with suffix '-operator')
-    beku_test_suite:    Beku test-suite to be run
-    operator_version:   Version of the operator to be tested
-    parallel:           How many parallel tests should be run
-    override_params:    additional (overriding) params
-                        Every option included in this string replaces the option value
-
+    operator:              name of the operator-repo (usually with suffix '-operator')
+    operator_version:      Version of the operator to be tested
+    parallel:              How many parallel tests should be run
+    test_script_params:    additional (overriding) params
+                           Every option included in this string replaces the default option value
     """ 
     params = ""
-    params += " --log-level debug" if "--log-level" not in override_params else ""
-    params += f" --test-suite {beku_test_suite}" if "--test-suite" not in override_params else ""
-    params += f" --operator {operator.replace('-operator','')}={operator_version}" if "--operator" not in override_params else ""
-    params += f" --parallel {parallel}" if "--parallel" not in override_params else ""
+    params += " --log-level debug" if "--log-level" not in test_script_params else ""
+    params += f" --operator {operator.replace('-operator','')}={operator_version}" if "--operator" not in test_script_params else ""
+    params += f" --parallel {parallel}" if "--parallel" not in test_script_params else ""
 
-    command = f"(cd {operator}/ && python ./scripts/run-tests {params} {override_params} 2>&1; echo $? > /test_exit_code) | tee {TEST_OUTPUT_LOGFILE}"
+    command = f"(cd {operator}/ && python ./scripts/run-tests {params} {test_script_params} 2>&1; echo $? > /test_exit_code) | tee {TEST_OUTPUT_LOGFILE}"
     log("Running the following test command:")
     log(command)
     os.system(command)
@@ -307,9 +303,8 @@ if __name__ == "__main__":
     sleep(60)
 
     log("Running tests...")
-    beku_test_suite = catalog['testsuites'][testsuite]['platforms'][platform]['beku-test-suite'] if 'beku-test-suite' in catalog['testsuites'][testsuite]['platforms'][platform] else 'nightly'
     parallel = catalog['testsuites'][testsuite]['parallel'] if 'parallel' in catalog['testsuites'][testsuite] else ''
-    test_exit_code = run_tests(testsuite, beku_test_suite, operator_version, parallel, override_beku_params)
+    test_exit_code = run_tests(testsuite, operator_version, parallel, test_script_params)
     log(f"Test exited with code {test_exit_code}")
     log()
 
