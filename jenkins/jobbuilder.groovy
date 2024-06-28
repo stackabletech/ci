b18da5a14285b98207380a1fabf1ca0d68048d3e
Nikolaus Winter <mail@nikolaus-winter.de>
2024-06-28 07:00:16 +0200

    Jenkins NG

--- jenkins/jobbuilder.groovy
+++ jenkins/jobbuilder.groovy
@@ -9,8 +9,20 @@ pipeline {
 
         stage('Log variables') {
             steps {
-                echo "Job Builder"
-                echo "Git branch: $BRANCH_NAME"
+                withCredentials([usernamePassword(credentialsId: '0823eb96-a01c-4e41-931d-c825d9065989', passwordVariable: 'JENKINS_BOT_PASSWORD', usernameVariable: 'JENKINS_BOT_USERNAME')]) {
+                    sh '''
+                        mkdir -p tools/jenkins-job-builder/catalog/
+                        cp catalog/*.yaml tools/jenkins-job-builder/catalog/
+
+                        docker build tools/jenkins-job-builder/ -t docker.stackable.tech/jenkins-job-builder:latest
+
+                        docker run --rm \
+                            --env JENKINS_URL=https://ci-dev.stackable.tech \
+                            --env JENKINS_USERNAME=$JENKINS_BOT_USERNAME \
+                            --env JENKINS_PASSWORD=$JENKINS_BOT_PASSWORD \
+                            docker.stackable.tech/jenkins-job-builder:latest
+                    ''' 
+                }            
             }
         }
 
