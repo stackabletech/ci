08d4c6c2483f620a2b2e2945d4955e7f9bacc313
Siegfried Weber <mail@siegfriedweber.net>
2025-07-16 16:14:06 +0200

    Disable cache in docker build

--- apps/build.sh
+++ apps/build.sh
@@ -10,7 +10,7 @@ rm -rf docker/operator-test-runner/src/
 cp -r src/ docker/operator-test-runner/src/
 
 # build Docker image
-docker build docker/operator-test-runner/ -t oci.stackable.tech/operator-test-runner:latest
+docker build --no-cache docker/operator-test-runner/ -t oci.stackable.tech/operator-test-runner:latest
 
 # copy resources
 rm -rf docker/jenkins-job-builder/catalog/
@@ -22,4 +22,4 @@ rm -rf docker/jenkins-job-builder/jjb/
 cp -r jjb/ docker/jenkins-job-builder/jjb/
 
 # build Docker image
-docker build docker/jenkins-job-builder/ -t oci.stackable.tech/jenkins-job-builder:latest
+docker build --no-cache docker/jenkins-job-builder/ -t oci.stackable.tech/jenkins-job-builder:latest
