69f0d25cce6258cb1045183cd28ac51692aceb3a
Lars Francke <git@lars-francke.de>
2025-12-02 21:59:29 +0100

    ruff format

--- apps/src/modules/catalog.py
+++ apps/src/modules/catalog.py
@@ -1,5 +1,5 @@
 """
-    This module enables access to the catalog
+This module enables access to the catalog
 """
 
 import hiyapyco
@@ -9,62 +9,66 @@ providers = []
 platforms = []
 operator_tests = []
 
+
 def read_providers(logger):
     """
-        Reads the list of cluster providers from platforms.yaml
+    Reads the list of cluster providers from platforms.yaml
 
-        logger              logger (String-consuming function)
+    logger              logger (String-consuming function)
     """
     global providers
     platforms_yaml = hiyapyco.load("/platforms.yaml")
-    if 'providers' not in platforms_yaml:
-        logger('platforms.yaml does not contain providers.')
+    if "providers" not in platforms_yaml:
+        logger("platforms.yaml does not contain providers.")
         return False
-    providers = platforms_yaml['providers']
+    providers = platforms_yaml["providers"]
     if len(providers) == 0:
-        logger('platforms.yaml does not contain providers.')
+        logger("platforms.yaml does not contain providers.")
         return False
     # TODO More syntax checks to make sure the catalog file is usable
     return True
+
 
 def read_platforms(logger):
     """
-        Reads the list of cluster platforms from platforms.yaml
+    Reads the list of cluster platforms from platforms.yaml
 
-        logger              logger (String-consuming function)
+    logger              logger (String-consuming function)
     """
     global platforms
-    platforms = hiyapyco.load("/platforms.yaml")['platforms']
+    platforms = hiyapyco.load("/platforms.yaml")["platforms"]
     platforms_yaml = hiyapyco.load("/platforms.yaml")
-    if 'platforms' not in platforms_yaml:
-        logger('platforms.yaml does not contain platforms.')
+    if "platforms" not in platforms_yaml:
+        logger("platforms.yaml does not contain platforms.")
         return False
-    platforms = platforms_yaml['platforms']
+    platforms = platforms_yaml["platforms"]
     if len(platforms) == 0:
-        logger('platforms.yaml does not contain platforms.')
+        logger("platforms.yaml does not contain platforms.")
         return False
     # TODO More syntax checks to make sure the catalog file is usable
     return True
 
+
 def read_operator_tests(logger):
     """
-        Reads the list of operator test definitions from operator-tests.yaml
+    Reads the list of operator test definitions from operator-tests.yaml
 
-        logger              logger (String-consuming function)
+    logger              logger (String-consuming function)
     """
     global operator_tests
     operator_tests = hiyapyco.load("/operator-tests.yaml")
     if len(operator_tests) == 0:
-        logger('operator-tests.yaml does not contain any tests.')
+        logger("operator-tests.yaml does not contain any tests.")
         return False
     # TODO More syntax checks to make sure the catalog file is usable
     return True
 
+
 def read_catalog(logger):
     """
-        Read all catalogs
+    Read all catalogs
 
-        logger              logger (String-consuming function)
+    logger              logger (String-consuming function)
     """
     if not read_providers(logger):
         return False
@@ -74,66 +78,74 @@ def read_catalog(logger):
         return False
     logger(f"Read {len(providers)} providers: [{','.join([p['id'] for p in providers])}]")
     logger(f"Read {len(platforms)} platforms: [{','.join([p['id'] for p in platforms])}]")
-    logger(f"Read {len(operator_tests)} operator tests: [{','.join([ot['id'] for ot in operator_tests])}]")
+    logger(
+        f"Read {len(operator_tests)} operator tests: [{','.join([ot['id'] for ot in operator_tests])}]"
+    )
     return True
+
 
 def get_platform(platform):
     """
-        Get the platform matching the platform string.
-        If the platform string matches an ID, that platform is chosen.
-        Otherwise, we search for a matching name attribute.
+    Get the platform matching the platform string.
+    If the platform string matches an ID, that platform is chosen.
+    Otherwise, we search for a matching name attribute.
 
-        platform        string which identifies a platform
+    platform        string which identifies a platform
     """
-    matching_platform = next(filter(lambda p: p['id']==platform, platforms), None)
+    matching_platform = next(filter(lambda p: p["id"] == platform, platforms), None)
     if not matching_platform:
-        matching_platform = next(filter(lambda p: p['name']==platform, platforms), None)
+        matching_platform = next(filter(lambda p: p["name"] == platform, platforms), None)
     return matching_platform
+
 
 def get_spec_for_operator_test(operator_test, platform, logger):
     """
-        Reads the cluster spec for the given operator/platform combination.
+    Reads the cluster spec for the given operator/platform combination.
 
-        The cluster spec is calculated combining up to 3 specs as following:
+    The cluster spec is calculated combining up to 3 specs as following:
 
-        1) Base spec: from platforms.yaml: 'spec' for the selected platform
-        2) Test-specific spec: Overwrite fields which are specified in the 'spec' section of the given operator test.
-        3) Platform-specific spec for operator test: Overwrite fields which are specified in the 'spec' section of the specific section for that platform inside the 'platforms' section of the operator test.
+    1) Base spec: from platforms.yaml: 'spec' for the selected platform
+    2) Test-specific spec: Overwrite fields which are specified in the 'spec' section of the given operator test.
+    3) Platform-specific spec for operator test: Overwrite fields which are specified in the 'spec' section of the specific section for that platform inside the 'platforms' section of the operator test.
 
-        operator_test:      ID of operator test
-        platform            ID of platform
+    operator_test:      ID of operator test
+    platform            ID of platform
 
-        Returns None and prints out an error message if the combination is not valid.
+    Returns None and prints out an error message if the combination is not valid.
     """
 
     # Find the matching platform, exit if not existing
-    matching_platform = next(filter(lambda p: p['id']==platform, platforms), None)
+    matching_platform = next(filter(lambda p: p["id"] == platform, platforms), None)
     if not matching_platform:
         logger(f"The platform '{platform}' does not exist.")
         return None
 
     # Find the matching operator test, exit if not existing
-    matching_operator_test = next(filter(lambda ot: ot['id']==operator_test, operator_tests), None)
+    matching_operator_test = next(
+        filter(lambda ot: ot["id"] == operator_test, operator_tests), None
+    )
     if not matching_operator_test:
         logger(f"The operator test '{operator_test}' does not exist.")
         return None
 
     # Find out the matching definition for the given platform inside the given operator test, exit if not existing
-    matching_operator_test_platform_def = next(filter(lambda p: p['id']==platform, matching_operator_test['platforms']), None)
+    matching_operator_test_platform_def = next(
+        filter(lambda p: p["id"] == platform, matching_operator_test["platforms"]), None
+    )
     if not matching_operator_test_platform_def:
         logger(f"The test for '{operator_test}' is not defined for the platform '{platform}'.")
         return None
 
     # The base spec from the platform
-    spec = matching_platform['spec']
+    spec = matching_platform["spec"]
 
     # If there are more specific specs in the operator test, overwrite the specs with these.
-    if 'spec' in matching_operator_test:
-        spec = {**spec, **matching_operator_test['spec']}
+    if "spec" in matching_operator_test:
+        spec = {**spec, **matching_operator_test["spec"]}
 
     # If there are more specific specs in the operator test for the given platform, overwrite the specs with these.
-    if 'spec' in matching_operator_test_platform_def:
-        spec = {**spec, **matching_operator_test_platform_def['spec']}
+    if "spec" in matching_operator_test_platform_def:
+        spec = {**spec, **matching_operator_test_platform_def["spec"]}
 
     return spec
 
@@ -146,7 +158,7 @@ def get_operator_test(operator_id):
 
     Returns the operator test dict or None if not found.
     """
-    return next(filter(lambda ot: ot['id'] == operator_id, operator_tests), None)
+    return next(filter(lambda ot: ot["id"] == operator_id, operator_tests), None)
 
 
 def get_test_script(operator_id):
@@ -162,7 +174,7 @@ def get_test_script(operator_id):
     if not operator_test:
         return "run-tests"  # default
 
-    return operator_test.get('test_script', 'run-tests')
+    return operator_test.get("test_script", "run-tests")
 
 
 def get_auto_retry_config(operator_id):
@@ -182,15 +194,11 @@ def get_auto_retry_config(operator_id):
     operator_test = get_operator_test(operator_id)
 
     # Default configuration
-    default_config = {
-        'attempts_parallel': 0,
-        'attempts_serial': 3,
-        'keep_failed_namespaces': False
-    }
+    default_config = {"attempts_parallel": 0, "attempts_serial": 3, "keep_failed_namespaces": False}
 
-    if not operator_test or 'auto_retry' not in operator_test:
+    if not operator_test or "auto_retry" not in operator_test:
         return default_config
 
     # Merge operator config with defaults
-    config = {**default_config, **operator_test['auto_retry']}
+    config = {**default_config, **operator_test["auto_retry"]}
     return config
--- apps/src/operator-test-runner.py
+++ apps/src/operator-test-runner.py
@@ -1,6 +1,7 @@
 """
-    Main module of the Operator Test Runner application
+Main module of the Operator Test Runner application
 """
+
 import os
 import sys
 import uuid
@@ -15,32 +16,32 @@ from modules.cluster_logging import install_cluster_logging
 from modules.command import run_command
 
 # values of the params (given as env vars during Docker run)
-param_output_file_user = '0:0'
+param_output_file_user = "0:0"
 param_platform = None
 param_platform_version = None
 param_operator = None
 param_git_branch = None
-param_test_script_params = ''
+param_test_script_params = ""
 param_cluster_logging_endpoint = None
 param_cluster_logging_username = None
 param_cluster_logging_password = None
 param_opensearch_dashboards_url = None
 
 # keys for the env vars
-PARAM_KEY_PLATFORM = 'PLATFORM'
-PARAM_KEY_PLATFORM_VERSION = 'PLATFORM_VERSION'
-PARAM_KEY_OPERATOR = 'OPERATOR'
-PARAM_KEY_OUTPUT_FILE_USER = 'OUTPUT_FILE_USER'
-PARAM_KEY_REPLICATED_API_TOKEN = 'REPLICATED_API_TOKEN'
-PARAM_KEY_IONOS_USERNAME = 'IONOS_USERNAME'
-PARAM_KEY_IONOS_PASSWORD = 'IONOS_PASSWORD'
-PARAM_KEY_GIT_BRANCH = 'GIT_BRANCH'
-PARAM_KEY_OPERATOR_VERSION = 'OPERATOR_VERSION'
-PARAM_KEY_TEST_SCRIPT_PARAMS = 'TEST_SCRIPT_PARAMS'
-PARAM_KEY_CLUSTER_LOGGING_ENDPOINT = 'CLUSTER_LOGGING_ENDPOINT'
-PARAM_KEY_CLUSTER_LOGGING_USERNAME = 'CLUSTER_LOGGING_USERNAME'
-PARAM_KEY_CLUSTER_LOGGING_PASSWORD = 'CLUSTER_LOGGING_PASSWORD'
-PARAM_KEY_OPENSEARCH_DASHBOARDS_URL = 'OPENSEARCH_DASHBOARDS_URL'
+PARAM_KEY_PLATFORM = "PLATFORM"
+PARAM_KEY_PLATFORM_VERSION = "PLATFORM_VERSION"
+PARAM_KEY_OPERATOR = "OPERATOR"
+PARAM_KEY_OUTPUT_FILE_USER = "OUTPUT_FILE_USER"
+PARAM_KEY_REPLICATED_API_TOKEN = "REPLICATED_API_TOKEN"
+PARAM_KEY_IONOS_USERNAME = "IONOS_USERNAME"
+PARAM_KEY_IONOS_PASSWORD = "IONOS_PASSWORD"
+PARAM_KEY_GIT_BRANCH = "GIT_BRANCH"
+PARAM_KEY_OPERATOR_VERSION = "OPERATOR_VERSION"
+PARAM_KEY_TEST_SCRIPT_PARAMS = "TEST_SCRIPT_PARAMS"
+PARAM_KEY_CLUSTER_LOGGING_ENDPOINT = "CLUSTER_LOGGING_ENDPOINT"
+PARAM_KEY_CLUSTER_LOGGING_USERNAME = "CLUSTER_LOGGING_USERNAME"
+PARAM_KEY_CLUSTER_LOGGING_PASSWORD = "CLUSTER_LOGGING_PASSWORD"
+PARAM_KEY_OPENSEARCH_DASHBOARDS_URL = "OPENSEARCH_DASHBOARDS_URL"
 
 # by convention, this is the return code for "unstable cluster"
 EXIT_CODE_CLUSTER_FAILED = 255
@@ -52,11 +53,12 @@ TEST_OUTPUT_LOGFILE = f"{TARGET_FOLDER}test-output.log"
 CLUSTER_INFO_FILE = f"{TARGET_FOLDER}cluster-info.txt"
 LOG_INDEX_LINKS_FILE = f"{TARGET_FOLDER}logs.html"
 
+
 def init():
     """
-        Initializes this app, checks if all params are provided as environment variables.
+    Initializes this app, checks if all params are provided as environment variables.
 
-        Returns True if initialization succeeded, False otherwise.
+    Returns True if initialization succeeded, False otherwise.
     """
     global param_platform
     global param_platform_version
@@ -99,15 +101,21 @@ def init():
         return False
 
     if PARAM_KEY_CLUSTER_LOGGING_ENDPOINT not in os.environ:
-        print(f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_ENDPOINT} as an environment variable.")
+        print(
+            f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_ENDPOINT} as an environment variable."
+        )
         return False
 
     if PARAM_KEY_CLUSTER_LOGGING_USERNAME not in os.environ:
-        print(f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_USERNAME} as an environment variable.")
+        print(
+            f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_USERNAME} as an environment variable."
+        )
         return False
 
     if PARAM_KEY_CLUSTER_LOGGING_PASSWORD not in os.environ:
-        print(f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_PASSWORD} as an environment variable.")
+        print(
+            f"Error: Please supply {PARAM_KEY_CLUSTER_LOGGING_PASSWORD} as an environment variable."
+        )
         return False
 
     param_platform = os.environ[PARAM_KEY_PLATFORM].strip()
@@ -139,31 +147,33 @@ def init():
 
 def set_target_folder_owner():
     """
-        As the Docker container is run with the root user (0:0), the files it produces will not be manageable by the Jenkins user.
-        That's why a UID/GID combo is to be specified as the OUTPUT_FILE_USER env var.
-        This method recursively sets the ownership of the output files.
+    As the Docker container is run with the root user (0:0), the files it produces will not be manageable by the Jenkins user.
+    That's why a UID/GID combo is to be specified as the OUTPUT_FILE_USER env var.
+    This method recursively sets the ownership of the output files.
     """
     os.system(f"chown -R {param_output_file_user} {TARGET_FOLDER}")
 
 
 def log(msg=""):
     """
-        Logs the given text message to stdout AND the logfile.
+    Logs the given text message to stdout AND the logfile.
     """
-    timestamp = f'{datetime.now(UTC):%Y-%m-%d %H:%M:%S}'
+    timestamp = f"{datetime.now(UTC):%Y-%m-%d %H:%M:%S}"
     print(f"{timestamp} :: {msg}")
     sys.stdout.flush()
     f = open(TESTDRIVER_LOGFILE, "a")
-    f.write(f'{timestamp} :: {msg}\n')
+    f.write(f"{timestamp} :: {msg}\n")
     f.close()
 
 
 def clone_git_repo(repo):
     """
-        Clones the given Stackable GitHub repo
+    Clones the given Stackable GitHub repo
     """
-    git_branch_option = f"-b { param_git_branch }" if param_git_branch else ""
-    exit_code, output = run_command(f"git clone {git_branch_option} https://github.com/stackabletech/{repo}.git", 'git clone')
+    git_branch_option = f"-b {param_git_branch}" if param_git_branch else ""
+    exit_code, output = run_command(
+        f"git clone {git_branch_option} https://github.com/stackabletech/{repo}.git", "git clone"
+    )
     if exit_code != 0:
         for line in output:
             log(line)
@@ -188,10 +198,10 @@ def run_tests(operator, operator_version, test_script_params):
 
     # Step 1: Installation of the SDP (retried max. 10 times to reduce flakiness)
     # This step is always done with run-tests regardless of the test script choice
-    command_install_sdp = f"cd {operator}/ && python ./scripts/run-tests --skip-tests --operator {operator.replace('-operator','')}={operator_version}"
+    command_install_sdp = f"cd {operator}/ && python ./scripts/run-tests --skip-tests --operator {operator.replace('-operator', '')}={operator_version}"
     log("Running the following command to install SDP for test:")
     log(command_install_sdp)
-    exit_code, output = run_command(command_install_sdp, 'install sdp', retries=10, delay=60)
+    exit_code, output = run_command(command_install_sdp, "install sdp", retries=10, delay=60)
     if exit_code != 0:
         for line in output:
             log(line)
@@ -199,18 +209,20 @@ def run_tests(operator, operator_version, test_script_params):
 
     # Step 2: Run the actual tests
     # (The aux. method run_command() is NOT used here because we want the output to be streamed, not captured!)
-    if test_script == 'auto-retry-tests.py':
+    if test_script == "auto-retry-tests.py":
         # Use auto-retry test runner
         retry_config = catalog.get_auto_retry_config(operator)
-        log(f"Auto-retry configuration: attempts_parallel={retry_config['attempts_parallel']}, "
+        log(
+            f"Auto-retry configuration: attempts_parallel={retry_config['attempts_parallel']}, "
             f"attempts_serial={retry_config['attempts_serial']}, "
-            f"keep_failed_namespaces={retry_config['keep_failed_namespaces']}")
+            f"keep_failed_namespaces={retry_config['keep_failed_namespaces']}"
+        )
 
         # Extract parallel value from test_script_params (default to 0)
         parallel_value = "0"
-        if '--parallel' in test_script_params:
+        if "--parallel" in test_script_params:
             try:
-                parts = test_script_params.split('--parallel')
+                parts = test_script_params.split("--parallel")
                 if len(parts) > 1:
                     parallel_value = parts[1].strip().split()[0]
             except:
@@ -227,7 +239,7 @@ def run_tests(operator, operator_version, test_script_params):
         ]
 
         # Add keep-failed-namespaces flag if configured to keep them
-        if retry_config['keep_failed_namespaces']:
+        if retry_config["keep_failed_namespaces"]:
             command_parts.append("--keep-failed-namespaces")
 
         # Add extra args from test_script_params (excluding --parallel which we already handled)
@@ -237,7 +249,7 @@ def run_tests(operator, operator_version, test_script_params):
             if skip_next:
                 skip_next = False
                 continue
-            if param == '--parallel':
+            if param == "--parallel":
                 skip_next = True
                 continue
             extra_params.append(param)
@@ -258,28 +270,37 @@ def run_tests(operator, operator_version, test_script_params):
     log(command_run_tests)
     os.system(command_run_tests)
     sleep(15)
-    with open ("/test_exit_code") as f:
+    with open("/test_exit_code") as f:
         return int(f.read().strip())
+
 
 def write_logs_html(cluster_id, timestamp_start, timestamp_stop, opensearch_dashboards_url):
     """
-        The output file 'logs.html' contains links to the OpenSearch Dashboards application which
-        are prepared to filter the matching cluster id and timeframe.
+    The output file 'logs.html' contains links to the OpenSearch Dashboards application which
+    are prepared to filter the matching cluster id and timeframe.
     """
 
     date_from = (timestamp_start - timedelta(hours=0, minutes=5)).strftime("%Y-%m-%dT%H:%M:00Z")
     date_to = (timestamp_stop + timedelta(hours=0, minutes=5)).strftime("%Y-%m-%dT%H:%M:00Z")
 
-    with open ("/src/modules/.cluster_logging/logs.html.j2") as f:
+    with open("/src/modules/.cluster_logging/logs.html.j2") as f:
         logs_html = Template(f.read())
 
-    with open (LOG_INDEX_LINKS_FILE, 'w') as f:
-        f.write(logs_html.render( { 'cluster_id': cluster_id, 'date_from': date_from, 'date_to': date_to, 'opensearch_dashboards_url': opensearch_dashboards_url } ))
+    with open(LOG_INDEX_LINKS_FILE, "w") as f:
+        f.write(
+            logs_html.render(
+                {
+                    "cluster_id": cluster_id,
+                    "date_from": date_from,
+                    "date_to": date_to,
+                    "opensearch_dashboards_url": opensearch_dashboards_url,
+                }
+            )
+        )
         f.close()
 
 
 if __name__ == "__main__":
-
     job_start_timestamp_utc = datetime.now(UTC)
 
     print("testing.stackable.tech operator-test-runner")
@@ -303,24 +324,35 @@ if __name__ == "__main__":
     if not platform:
         log(f"The platform '{param_platform}' does not exist.")
         exit(EXIT_CODE_CLUSTER_FAILED)
-    if param_platform_version not in platform['versions']:
-        log(f"The version '{param_platform_version}' does not exist for platform '{param_platform}'.")
+    if param_platform_version not in platform["versions"]:
+        log(
+            f"The version '{param_platform_version}' does not exist for platform '{param_platform}'."
+        )
         exit(EXIT_CODE_CLUSTER_FAILED)
     log(f"Test running on platform '{platform['id']}', version {param_platform_version}.")
 
     # Read the cluster spec for the given platform
-    cluster_spec = catalog.get_spec_for_operator_test(param_operator, platform['id'], log)
+    cluster_spec = catalog.get_spec_for_operator_test(param_operator, platform["id"], log)
     if not cluster_spec:
         log("Cluster spec could not be determined.")
         exit(EXIT_CODE_CLUSTER_FAILED)
 
-    log(f"Test running on Git Branch {param_git_branch} with the test script parameters '{param_test_script_params}'...")
+    log(
+        f"Test running on Git Branch {param_git_branch} with the test script parameters '{param_test_script_params}'..."
+    )
 
     # random cluster ID
     cluster_id = uuid.uuid4().hex
 
     log("Creating cluster...")
-    cluster = create_cluster(platform['provider'], cluster_id, cluster_spec, param_platform_version, CLUSTER_INFO_FILE, log)
+    cluster = create_cluster(
+        platform["provider"],
+        cluster_id,
+        cluster_spec,
+        param_platform_version,
+        CLUSTER_INFO_FILE,
+        log,
+    )
     if not cluster:
         log("Cluster could not be created.")
         exit(EXIT_CODE_CLUSTER_FAILED)
@@ -332,7 +364,13 @@ if __name__ == "__main__":
     sleep(60)
 
     log("Install Cluster Logging (powered by Vector)...")
-    installed_cluster_logging = install_cluster_logging(cluster_id, param_cluster_logging_endpoint, param_cluster_logging_username, param_cluster_logging_password, log)
+    installed_cluster_logging = install_cluster_logging(
+        cluster_id,
+        param_cluster_logging_endpoint,
+        param_cluster_logging_username,
+        param_cluster_logging_password,
+        log,
+    )
     if installed_cluster_logging:
         log("Installed Cluster Logging (powered by Vector).")
     else:
@@ -350,13 +388,18 @@ if __name__ == "__main__":
     log("Waiting 1 minute to allow logs to be processed...")
     sleep(60)
 
-    termination_successful = terminate_cluster(platform['provider'], cluster, log)
+    termination_successful = terminate_cluster(platform["provider"], cluster, log)
 
     job_finished_timestamp_utc = datetime.now(UTC)
 
     # Write file which links to the logs if URL set
     if param_opensearch_dashboards_url:
-        write_logs_html(cluster_id, job_start_timestamp_utc, job_finished_timestamp_utc, param_opensearch_dashboards_url)
+        write_logs_html(
+            cluster_id,
+            job_start_timestamp_utc,
+            job_finished_timestamp_utc,
+            param_opensearch_dashboards_url,
+        )
 
     # Set output file ownership recursively
     # This is important as the test script might have added files which are not controlled
